7972d3f92ab512339b7b1d43a6080990546847c5
ahmadfahmisugati@gmail.com <ahmadfahmisugati@gmail.com>
2021-09-30 17:52:12 +0700

    30

--- config/database.js
+++ config/database.js
@@ -1,8 +1,13 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize('express','root','',{
-    host : "localhost",
-    dialect : "mysql"
+    host : 'localhost',
+    dialect : 'mysql',
+    pool:{
+        max:5,
+        min:0,
+        idle:10000
+    }
 })
 
 export default db;
